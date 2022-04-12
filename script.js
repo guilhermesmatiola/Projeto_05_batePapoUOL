3d79a422226c361ff981e969c18d891bef5f1f78
guischlindwein <guilherme.matiola@integrejr.com>
2022-04-12 15:29:32 -0300

    Funções de inserção de texto no js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+function inChat(){
 
+    document.querySelector(".messages").innerHTML+=`
+    <div class="in-out-room">
+      <h1><strong>pessoa </strong>entrou na sala... </h1>
+    </div>
+    `
+
+}
+
+function outChat(){
+
+    document.querySelector(".messages").innerHTML+=`
+    <div class="in-out-room">
+      <h1><strong>pessoa </strong>entrou na sala... </h1>
+    </div>
+    `
+
+}
+
+function privateChat(){
+
+    document.querySelector(".messages").innerHTML+=`
+    <div class="private-message">
+         <h1><strong>pessoa</strong> para <strong>pessoa</strong>:</h1>
+    </div>
+    `
+
+}
+
+function allChat(){
+
+    document.querySelector(".messages").innerHTML+=`
+    <div class="public-message">
+        <h1><strong>pessoa</strong> para <strong>Todos</strong>:</h1>
+    </div>
+    `
+}
+privateChat();
+inChat();
+outChat();
+privateChat();
+allChat();
