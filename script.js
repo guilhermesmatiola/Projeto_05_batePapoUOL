e21e4b8509c7d94cdde82467f0ae52dd1f5feefe
guischlindwein <guilherme.matiola@integrejr.com>
2022-04-13 09:19:33 -0300

    Chama usuário e verifica se tem ou não. Sucess e error

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+userDefinition();
+
+function userDefinition(){
+    userName = prompt("Qual o seu nome?");
+    let data={
+        name: userName
+    };
+
+    let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',data);
+    promise.then(sucess);
+    promise.catch(failed);
+}
+function failed(error){
+
+    if(error.response.status===400){
+        alert('O nome de usuário fornecido já está em uso, escolha outro nome.')
+    }
+    userDefinition();
+
+}
+
+function sucess(){
+    console.log("sucess");
+
+    privateChat();
+    inChat();
+    outChat();
+    privateChat();
+    allChat();
+    privateChat();
+    inChat();
+    outChat();
+    privateChat();
+    allChat();
+    privateChat();
+    inChat();
+    outChat();
+    privateChat();
+    allChat();
+    privateChat();
+    allChat();
+    privateChat();
+    inChat();
+    outChat();
+    privateChat();
+    allChat();
+    privateChat();
+    allChat();
+    privateChat();
+    inChat();
+    outChat();
+    privateChat();
+    allChat();
+
+}
+
+
+
+
 function inChat(){
 
     document.querySelector(".messages").innerHTML+=`
@@ -12,7 +71,7 @@ function outChat(){
 
     document.querySelector(".messages").innerHTML+=`
     <div class="in-out-room">
-      <h1><strong>pessoa </strong>entrou na sala... </h1>
+      <h1><strong>pessoa </strong>saiu da sala... </h1>
     </div>
     `
 
@@ -36,8 +95,3 @@ function allChat(){
     </div>
     `
 }
-privateChat();
-inChat();
-outChat();
-privateChat();
-allChat();
